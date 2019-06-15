d9da457de34d7676287866b683bedcf526523cf1
qingge <1823295186@qq.com>
2019-06-15 20:29:18 +0800

    0615

--- 第三阶段课程/06-react/03-todolist-wodelianxi/src/App.js
+++ 第三阶段课程/06-react/03-todolist-wodelianxi/src/App.js
@@ -1,9 +1,4 @@
-/*
-* @Author: TomChen
-* @Date:   2019-04-09 19:29:30
-* @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-10 20:43:50
-*/
+
 
 import React,{ Component,Fragment } from 'react'
 import { DatePicker,Button } from 'antd';
@@ -18,53 +13,72 @@ class App extends Component{
 		this.state = {
 			list:["吃饭","睡觉","情歌"],
 			value:''
-
 		}
+		this.handleChange = this.handleChange.bind(this)
+		this.handleAdd = this.handleAdd.bind(this)
 	}
 	handleAdd(){
 		// console.log('aaa...')
 		// this.state.list.push(this.state.value)
-		this.setState({
-			list:[...this.state.list,this.state.value],
-			value:''
-		})
+		// this.setState({
+		// 	list:[...this.state.list,this.state.value],
+		// 	value:''
+		// })
+		// this.setState(()=>{
+		// 	return {
+		// 		list:[...this.state.list,this.state.value],
+		//  		value:''
+		// 	}
+		// })
+		// this.setState((preState)=>{
+		// 	return {
+		// 		list:[...preState.list,preState.value],
+		//  		value:''
+		// 	}
+		// })
+		this.setState(preState=>({
+				list:[...preState.list,preState.value],
+		 		value:''
+		}))
 	}
 	handleChange(ev){
 		// console.log(ev.target.value)
 		// this.state.value = ev.target.value
-		this.setState({
-			value:ev.target.value,
-		})
-	}
+		// this.setState({
+		// 	value:ev.target.value,
+		// })
+		const value = ev.target.value
+		this.setState(()=>({
+			value:value
+		}));
+	}                       
 	handleDelet(index){
 		// console.log('del...',index)
 		// this.state.list.splice(index,1)
 		const list = [...this.state.list]
 		list.splice(index,1)
 		console.log(list)
-		this.setState({
-			list:list
-		})
+		// this.setState({
+		// 	list:list
+		// })
+		this.setState(()=>({
+			list
+		}))
 
+	}
+	getItems(){
+		return this.state.list.map((item,index)=>{
+			return <Item key={index} content={item} handleDelet={this.handleDelet.bind(this,index)} />
+		})
 	}
 	render(){
 		return(
 			<div className="App">
-				<input onChange={this.handleChange.bind(this)} value={this.state.value}/>
-				<button onClick={this.handleAdd.bind(this)}>新增</button>
+				<input onChange={this.handleChange} value={this.state.value}/>
+				<button onClick={this.handleAdd}>新增</button>
 				<ul>
 					{
-						this.state.list.map((item,index)=>{
-							// return(
-							// 	<li 
-							// 		key={index}
-							// 		onClick={this.handleDelet.bind(this,index)}
-							// 	>
-							// 	{item}
-							// 	</li>
-							// )
-							return <Item key={index} content={item} list={this.state.list} />
-						})
+						this.getItems()
 					}
 				</ul>
 			</div>
--- 第三阶段课程/06-react/03-todolist-wodelianxi/src/Item.js
+++ 第三阶段课程/06-react/03-todolist-wodelianxi/src/Item.js
@@ -1,26 +1,25 @@
-/*
-* @Author: TomChen
-* @Date:   2019-04-09 20:41:19
-* @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-10 20:37:05
-*/
+
 import React,{ Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Item extends Component{
-	constructor(props){
-		super(props);
-	}
-	handleDel(){
-		console.log(this.props.list)
-	}
 	render(){
+		//结构赋值
+		const {handleDelet,content} = this.props;
 		return (
-			<li onClick={this.handleDel.bind(this)}>
-				{this.props.content}
+			<li onClick={handleDelet}>
+				{content}
 			</li>
 		)
 	}
+}
+Item.propTypes = {
+	handleDelet:PropTypes.func,
+	content:PropTypes.string.isRequired
+	// isRequired必须项必须传参
+}
+Item.DefaultProps = {
+	content:'睡觉'
 }
 
 export default Item
